7b594721212b59e8d776e026da61da596defd6ba
Josue Martinez <jhiram.alvz@gmail.com>
2023-06-13 21:03:48 -0600

    Modified tests watchAll

--- React/02-counter-app/src/tests/base-pruebas/08-imp-exp.test.js
+++ React/02-counter-app/src/tests/base-pruebas/08-imp-exp.test.js
@@ -1,3 +1,9 @@
+import { getHeroeById } from "../../base-pruebas/08-imp-exp";
+
 describe('Prueba en 08-imp-exp', () => { 
-    
+    test('getHeroeById debería de retornar un Heroe', () => { 
+        const id = 1;
+        const heroe = getHeroeById(id);
+        console.log(heroe)
+     })
  })
